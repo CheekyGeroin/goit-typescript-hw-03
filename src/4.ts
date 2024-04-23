80283cf8f6ece6400e713cfe0609611ff332911c
CheekyGeroin <dan.97198@gmail.com>
2024-04-23 13:10:13 +0200

    done tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = false;
+  public key: Key;
+  public tenants: Person[] = [];
+
+    constructor(key:Key) {
+      this.key = key
+  }
+
+  comeIn(person:Person):void {
+      if (this.door) {
+        this.tenants.push(person)
+      console.log("Welcome home!");
+    } else {
+        console.log("The door is closed")
+    }
+  }
+
+  abstract openDoor(key: Key):void
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+      if (key.getSignature() === this.getHouseKey().getSignature()) {
+          this.door= true
+      }
+  }
+    getHouseKey():Key {
+        return (this as any).key
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
